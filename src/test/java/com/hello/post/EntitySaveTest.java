7527e8b2690fb1fab0dca632d764208eb29c1781
jangwon kim <seed0335@gmail.com>
2023-07-12 17:26:51 +0900

    엔티티 저장 테스트 케이스 작성완료 및 테스트 성공

---
+++ src/test/java/com/hello/post/EntitySaveTest.java
@@ -0,0 +1,49 @@
+package com.hello.post;
+
+import com.hello.post.entity.Comment;
+import com.hello.post.entity.Like;
+import com.hello.post.entity.Post;
+import com.hello.post.entity.User;
+import com.hello.post.repository.CommentRepository;
+import com.hello.post.repository.PostRepository;
+import com.hello.post.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+@SpringBootTest
+public class EntitySaveTest {
+
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    PostRepository postRepository;
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    @Rollback(value = false)
+    void save1() {
+        //유저 저장 테스트
+        User user = new User();
+        user.setUsername("seed");
+        user.setPassword("0335");
+        userRepository.save(user);
+
+        //포스트 저장 테스트
+        Post post = new Post();
+        post.setTitle("오늘의 할일");
+        post.setContent("개인과제를 해야 합니다.");
+        post.setLike(Like.On);
+        post.setUser(user);
+        postRepository.save(post);
+
+        //댓글 저장 테스트
+        Comment comment = new Comment();
+        comment.setComment("화이팅입니다.");
+        comment.setUser(user);
+        comment.setPost(post);
+        commentRepository.save(comment);
+    }
+}
